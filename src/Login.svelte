fd4f615629185e94ec85141ee13c6bf4212a16e6
Jose Antonio Muñoz Jimenez <jamj2000@gmail.com>
2020-05-25 21:03:11 +0200

    Mostramos error de login en pagina

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,15 +1,14 @@
 <script>
-  import {  refreshToken }  from "./store.js";
+  import { refreshToken } from "./store.js";
   let usuario = {};
-  let mensaje = '';
-  let accessToken = '';
+  let mensaje = "";
+  let accessToken = "";
 
   // let URL = "http://localhost:3000";
 
   let URL = "https://jwt-back.herokuapp.com";
 
-
-  function registrar () {
+  function registrar() {
     fetch(URL + "/auth/signup", {
       method: "POST",
       headers: {
@@ -18,9 +17,11 @@
       body: JSON.stringify(usuario)
     })
       .then(res => res.json())
-      .then(data => { mensaje = data.msg; setTimeout(() => (mensaje = ''), 3000); })
+      .then(data => {
+        mensaje = data.msg;
+        setTimeout(() => (mensaje = ""), 3000);
+      })
       .catch(err => console.log(err));
-
   }
 
   function obtenerToken() {
@@ -32,42 +33,42 @@
       body: JSON.stringify(usuario)
     })
       .then(res => res.json())
-      .then(token =>  { 
-        accessToken = token; 
-        console.log(accessToken) 
+      .then(data => {
+        if (data.msg) {
+          mensaje = data.msg;
+          setTimeout(() => (mensaje = ""), 4000);
+        } else {
+          accessToken = data;
+          console.log(accessToken);
+        }
       })
-      .catch(err => {
-        mensaje = err.msg;
-        setTimeout(() => (mensaje = ''), 8000);  
-        console.log(err)
-      });
+      .catch(err => console.log(err));
 
     //await login({ jwt_token })
     // console.log(jwt_token);
   }
 
-
-async function getClientes() {
+  async function getClientes() {
     let authorization = "Bearer " + accessToken;
     let res = await fetch(URL + "/api/clientes", {
       method: "GET",
       headers: {
-        "Authorization": authorization
+        Authorization: authorization
       }
     });
-    
-    if (res.status == 403 || res.status == 401) { console.log('Acceso denegado o sin acceso'); } 
-    
-    let data = await res.json();   
+
+    if (res.status == 403 || res.status == 401) {
+      console.log("Acceso denegado o sin acceso");
+    }
+
+    let data = await res.json();
     if (data) {
-      mensaje = JSON.stringify(data, null, 2); 
-      setTimeout(() => (mensaje = ''), 8000);  
+      mensaje = JSON.stringify(data, null, 2);
+      setTimeout(() => (mensaje = ""), 8000);
       console.log(data);
-      }
-     
+    }
   }
 </script>
-
 
 <div>
   <h1>Iniciar sesión</h1>
@@ -75,19 +76,20 @@ async function getClientes() {
     bind:value={usuario.email}
     type="text"
     name="email"
-    placeholder="email"
- />
+    placeholder="email" />
   <br />
   <input
     bind:value={usuario.password}
     type="password"
     name="password"
-    placeholder="password"
- />
+    placeholder="password" />
   <br />
   <input type="button" value="Registrarme" on:click={registrar} />
-  <input type="button" value="Iniciar sesión" on:click={obtenerToken} /><br>
+  <input type="button" value="Iniciar sesión" on:click={obtenerToken} />
+  <br />
 
   <input type="button" value="Obtener clientes" on:click={getClientes} />
 </div>
-<pre id="feedback" style='text-align: left;'>{@html mensaje}</pre>
+<pre id="feedback" style="text-align: left;">
+  {@html mensaje}
+</pre>
