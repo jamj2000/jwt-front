c7ab58eafbca0179970e1724de92e458c22f6fc0
Jose Antonio Muñoz Jimenez <jamj2000@gmail.com>
2020-05-25 20:09:27 +0200

    Refactorizacion

---
+++ src/Login.svelte
@@ -0,0 +1,96 @@
+<script>
+  import {  refreshToken }  from "./store.js";
+  let usuario = {};
+  let mensaje = '';
+  let accessToken = '';
+
+  let URL = "http://localhost:3000";
+
+
+
+  function registrar () {
+    fetch(URL + "/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(usuario)
+    })
+      .then(res => res.json())
+      .then(data => { mensaje = data.msg; setTimeout(() => (mensaje = ''), 3000); })
+      .catch(err => console.log(err));
+
+  }
+
+  function obtenerToken() {
+    fetch(URL + "/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(usuario)
+    })
+      .then(res => res.json())
+      .then(token =>  { accessToken = token; console.log(accessToken) })
+      .catch(err => console.log(err));
+
+    //await login({ jwt_token })
+    // console.log(jwt_token);
+  }
+
+//   function getClientes() {
+//     fetch(URL + "/api/clientes", {
+//       method: "GET",
+//       headers: {
+//         Authorization: "Bearer " + accessToken
+//       }
+//     })
+//       .then(res => {if (res.status == 401)  console.log('Sin permiso'); res.json()} )
+//       .then(data => {
+//         console.log(data);        
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+ async function getClientes() {
+    let authorization = "Bearer " + accessToken;
+    let res = await fetch(URL + "/api/clientes", {
+      method: "GET",
+      headers: {
+        "Authorization": authorization
+      }
+    });
+    
+    if (res.status == 403 || res.status == 401) { console.log('Acceso denegado o sin acceso'); } 
+    
+    let data = await res.json();   
+    if (data) console.log(data);
+     
+  }
+</script>
+
+
+<div>
+  <h1>Iniciar sesión</h1>
+  <input
+    bind:value={usuario.email}
+    type="text"
+    name="email"
+    placeholder="email"
+    tabindex="-2" />
+  <br />
+  <input
+    bind:value={usuario.password}
+    type="password"
+    name="password"
+    placeholder="password"
+    tabindex="-1" />
+  <br />
+  <input type="button" value="Registrarme" on:click={registrar} />
+  <input type="button" value="Iniciar sesión" on:click={obtenerToken} /><br>
+
+  <input type="button" value="Obtener clientes" on:click={getClientes} />
+</div>
+<div id="feedback">{@html mensaje}</div>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+ 
+import { writable } from 'svelte/store';
+
+export const accessToken = writable([]);
+export const refreshToken = writable([]);
